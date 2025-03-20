9de58c3bcaff99b88e417ca8305afca3c4e756ba
matchonerror <phuvnqn24@gmail.com>
2025-03-20 16:26:44 +0100

    2

--- include/snakewindow.hpp
+++ include/snakewindow.hpp
@@ -5,19 +5,35 @@
 #include <QtWidgets/QtWidgets>
 #include "jeu.hpp"
 
-class SnakeWindow  : public QFrame
+class SnakeWindow : public QFrame
 {
   protected:
     Jeu jeu;
     QPixmap pixmapCorps, pixmapTete, pixmapMur;
-
+    int decalageY;
+    
   public:
     SnakeWindow(QWidget *pParent=nullptr, Qt::WindowFlags flags=Qt::WindowFlags());
-    void handleTimer();
-
+    
   protected:
     void paintEvent(QPaintEvent *);
     void keyPressEvent(QKeyEvent *);
+    void handleTimer();
+    void handleButtonAjout();
+    void handleButtonSuppr();
+};
+
+class SnakeButton : public QPushButton
+{
+  public:
+    SnakeButton(QWidget *pParent=0):QPushButton(pParent) {}
+
+  protected:
+    void keyPressEvent(QKeyEvent *e)
+    {
+        if (parent()!=NULL)
+            QCoreApplication::sendEvent(parent(), e);
+    }
 };
 
 #endif
